9b233ea4f43fd7d391649089c7c14e1f084b7d2c
Vandr0iy <vandr0iy@pm.me>
2022-05-22 00:38:35 +0200

    prerequisites

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nix flake";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            docker
+            hugo
+          ];
+        };
+      }
+    );
+}
+
